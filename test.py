26f400eaf60a4cc4372a186ed2b01185ac21cd81
Di Wu <xiaowu200031@gmail.com>
2021-08-04 17:50:51 +0800

    code sync

--- test.py
+++ test.py
@@ -4,26 +4,15 @@
 import torch
 import numpy as np
 
-from nni.compression.pytorch.utils.counter import count_flops_params
-
-from mobilenet import MobileNet
-from mobilenet_v2 import MobileNetV2
+from utils import *
 
 
-def main(mobilenet_version='v1'):
-    assert mobilenet_version in ['v1', 'v2']
-
-    if mobilenet_version == 'v1':
-        model = MobileNet(n_class=1000, profile='normal')
-    else:
-        model = MobileNetV2(n_class=1000, input_size=224, width_mult=1.)
-
+def main(model='mobilenet_v1', checkpoint=None, pretrained=False):
+    model = create_model(model_type=model, checkpoint=checkpoint, pretrained=pretrained)
     print(model)
-    
-    dummy_input = torch.rand([1, 3, 256, 256])
-    flops, params, results = count_flops_params(model, dummy_input)
-    print(f"FLOPs: {flops}, params: {params}")
+    count_flops(model)
+
 
 
 if __name__ == '__main__':
-    main('v2')
+    main('mobilenet_v2', pretrained=False)
